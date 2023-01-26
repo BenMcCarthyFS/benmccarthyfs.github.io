75b2dab7fbf3e3d12833d41d79b40dd1442359b0
BenMcCarthy <ben.mccarthy@fleetsimts.ca>
2023-01-26 08:33:40 -0400

    Initial test

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let maxAngleDeg = 2;
+let mouseOverContainer = document.body;
+let ex1Layer = document.getElementById("backgroundSplash");
+
+let _root = document.querySelector(':root');
+
+function transforms(x, y, el) {
+    let box = el.getBoundingClientRect();
+
+    ((x - (window.innerWidth / 2)) / window.innerWidth) * maxAngleDeg;
+
+
+    let calcX = ((y - (window.innerHeight / 2)) / window.innerHeight) * maxAngleDeg;
+    let calcY = -((x - (window.innerWidth / 2)) / window.innerWidth) * maxAngleDeg;
+
+    _root.style.setProperty("--bg-rotate-x", calcX+"deg");
+    _root.style.setProperty("--bg-rotate-y", calcY+"deg");
+};
+
+function transformElement(el, xyEl) {
+    transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function (e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([ex1Layer]);
+    window.requestAnimationFrame(function () {
+        transformElement(ex1Layer, position);
+    });
+};
