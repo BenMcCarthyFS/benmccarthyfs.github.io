d97c600c19badf6f3ef9a330f4097637f6a2fe24
BenMcCarthy <ben.mccarthy@fleetsimts.ca>
2023-03-01 11:11:51 -0400

    -SVG logo updated to show correct font and spacing -nav js updated to highlight current page -addons page added

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@
     let _navBarText =   `<div class="logo">
                         <a href="/index.html">
                         <span>
-                        <object type="image/svg+xml" data="images/RCN_VF_Logo_20230217.svg"></object>
+                        <object type="image/svg+xml" data="images/RCN_VF_Logo_20230301_Light_Expanded.svg"></object>
                         </span>`+
                         /*<img src="images/RCN_VF_Logo_20230217.svg" alt="Alternate Text" />`+
                        /* <div>
@@ -13,16 +13,36 @@
                     <input type="checkbox" id="nav-toggle" class="nav-toggle" value="" />
                     <nav>
                         <ul>
-                            <li><a href="">Add Ons</a></li>
-                            <li><a href="community.html">Community</a></li>
-                            <li><a href="help.html">Help</a></li>
-                            <li><a href="loginSignUp.html">Login</a></li>
+                            <li><a href="addOns.html" data-name='addons'>Add Ons</a></li>
+                            <li><a href="community.html" data-name='community'>Community</a></li>
+                            <li><a href="help.html" data-name='help'>Help</a></li>
+                            <li><a href="loginSignUp.html" data-name='login'>Login</a></li>
                         </ul>
                     </nav>
                     <label for="nav-toggle" class="nav-toggle-label">
                         <span></span>
                     </label>`;
 
+
+let _navWrapper = document.createElement("header");
+_navWrapper.innerHTML = _navBarText;
+
+
 let _oldElement = document.querySelector("script#replace_with_navbar");
-    _oldElement.parentElement.innerHTML = _navBarText;
+
+let _navCurrent = _oldElement.dataset.name;
+
+if(_navCurrent != undefined)
+{
+try {
+    _navWrapper.querySelector(`[data-name="` + _navCurrent + `"]`).dataset.state = "active";
+} catch (error) {
+    
+}
+}
+_oldElement.replaceWith(_navWrapper);
+
+/*
+let _oldElement = document.querySelector("script#replace_with_navbar");
+    _oldElement.parentElement.innerHTML = _navBarText;*/
 })();
